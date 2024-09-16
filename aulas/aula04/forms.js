eb8a7a863ef4697f76641ec02007089e10a11f66
Gustavo Lima <luis.lima133@etec.sp.gov.br>
2024-09-16 18:18:28 -0300

    JS Corrijido

--- aulas/aula04/forms.js
+++ aulas/aula04/forms.js
@@ -1,40 +1,40 @@
-let proximoID = 1
+let proximoId = 1
 
 function adicionarLinha(){
-    //Entrada de valores do fomulário
-    const nome = document.getElementById("nome").value; 
+    //Entrada de valores do formulário
+    const nome = document.getElementById("nome").value;
     const idade = document.getElementById("idade").value;
     const admissao = document.getElementById("admissao").value;
     const demissao = document.getElementById("demissao").value;
 
     //Se for preenchido vazio
     if(nome === '' || idade === '' || admissao === ''){
-        alert("Preencha os valores obrigatórios")
-    }
+    alert("Preencha os valores do formulário!")
+    } else{ //Se for preenchido corretamente
 
     //Cria uma linha da tabela se não existir
-    const tabela = document.getElementById("tabelaDados").getElementsByTagName("tbody") [0];
+    const tabela = document.getElementById("tabelaDados").getElementsByTagName('tbody')[0];
 
     //Inserindo uma nova linha
-    const novaLinha = tabela.insertRow;
+    const novaLinha = tabela.insertRow();
 
     //Inserindo os valores da linha
-    const celID = novaLinha.insertCell (0);
+    const celId = novaLinha.insertCell(0);
     const celNome = novaLinha.insertCell(1);
     const celIdade = novaLinha.insertCell(2);
     const celAdmissao = novaLinha.insertCell(3);
     const celDemissao = novaLinha.insertCell(4);
 
     //Inserindo os valores dentro das celulas
-    celID.innerhHTML = proximoID;
-    celNome.innerhHTML = nome;
-    celIdade.innerhHTML = idade;
-    celAdmissao.innerhHTML = admissao;
-    celDemissao.innerhHTML = demissao;
+    celId.innerHTML = proximoId;
+    celNome.innerHTML = nome;
+    celIdade.innerHTML = idade;
+    celAdmissao.innerHTML = admissao;
+    celDemissao.innerHTML = demissao;
 
-    proximoID++
+    proximoId++
 
-    //Limpar formulário
+    //Limpar formulario
     document.getElementById("linhaForm").reset();
-
+}
 }
